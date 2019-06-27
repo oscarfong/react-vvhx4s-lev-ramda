bf1bb133f1ce89d022c8b7a3e39834c0953b1e9a
oscarfong <oscar.fong@gmail.com>
2019-06-27 14:27:24 +0800

    Table (bootstrap) was successfully populated with getBSRows

--- LevStrStore.js
+++ LevStrStore.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import * as R from 'ramda';
 
-import {Button}  from 'react-bootstrap';
+import {Button,Table}  from 'react-bootstrap';
 
 class LevStrStore extends Component{
   constructor() {
@@ -35,8 +35,45 @@ class LevStrStore extends Component{
     return xpwd;
   }
 
+  getBSRows() {
+    const fnRow = rr =>   <tr>
+                  <td>*</td>
+                  <td>{rr[0]}</td>
+                  <td>{rr[1]}</td>
+                  <td>{rr[2]}</td>
+                </tr> ;
+    const btRows = R.map(fnRow, this.getXProdWithScore());
+
+    return btRows;
+  }
+
   getBSTable() {
-    return <Button variant="primary">Primary</Button>;
+    return 	<div>
+              <Button variant="primary">Primary</Button>
+              <Table striped bordered hover>
+                <thead>
+                <tr>
+                  <th>#</th>
+                  <th>str1</th>
+                  <th>str2</th>
+                  <th>score</th>
+                </tr>
+                </thead>
+                <tbody>
+                
+                <tr>
+                  <td>1</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+
+                {this.getBSRows()}
+                
+                </tbody>
+              </Table>
+            </div>
+    ;
   }
 
   render() {
