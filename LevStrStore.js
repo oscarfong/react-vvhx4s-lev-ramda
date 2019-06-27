271543417a3c2f169d74d24f39b665d05eded683
oscarfong <oscar.fong@gmail.com>
2019-06-27 12:14:47 +0800

    LevStrStore/getXProd

--- LevStrStore.js
+++ LevStrStore.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import * as R from 'ramda';
+
 class LevStrStore extends Component{
   constructor() {
     super();
@@ -9,8 +11,22 @@ class LevStrStore extends Component{
     return ["apple", "orange", "banana" ];
   }
 
+  //  LevStrStore/getXProd
+  getXProd() {
+    const xp= R.xprod( this.getStringArray(), this.getStringArray());
+
+    console.log(xp);
+
+
+    return xp;
+  }
+
   render() {
-    return <div> {this.getStringArray().join(",")} </div>
+    const ans = this.getXProd().join(",");
+    
+    return <div> { ans } </div>
+    // return <div> {this.getStringArray().join(",")} </div>
+    
   }
   
 
