3a0d64961ce1b3ada3f77a54b895e426fb44f7ad
oscarfong <oscar.fong@gmail.com>
2019-06-27 16:17:34 +0800

    formSkeletonNotTested added

--- BTTest.js
+++ BTTest.js
@@ -11,9 +11,32 @@ class BTTest extends Component {
   }
 
 
-  render() {
-    return (
-    <div>
+  // formSkeletonNotTested() {
+  //   return (
+  //     <Form>
+  //       <FormGroup
+  //           controlId="formBasicText"
+  //           validationState={this.getValidationState()}
+  //         >
+  //           <ControlLabel>Working example with validation</ControlLabel>
+  //           <FormControl
+  //             type="text"
+  //             value={this.state.value}
+  //             placeholder="Enter text"
+  //             onChange={this.handleChange}
+  //           />
+  //           <FormControl.Feedback />
+  //           <HelpBlock>Validation is based on string length.</HelpBlock>
+  //         </FormGroup>
+  //     </Form>
+  //    );
+  // }
+
+
+
+
+  formSkeleton() {
+	return (
       <Form>
         <FormGroup
             controlId="formBasicText"
@@ -29,9 +52,14 @@ class BTTest extends Component {
             <FormControl.Feedback />
           </FormGroup>
       </Form>
+	 );
+  }
 
 
-
+  render() {
+    return (
+    <div>
+       {this.formSkeleton()}
     </div>
 	
 	
