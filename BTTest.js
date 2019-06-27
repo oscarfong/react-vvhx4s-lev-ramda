6530092a64e165a18bcd2adec43ccfd9aa807abf
oscarfong <oscar.fong@gmail.com>
2019-06-27 15:47:21 +0800

    BTTest.js skeleton : 1 file added

---
+++ BTTest.js
@@ -0,0 +1,18 @@
+// react-bootstrap test.
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class BTTest extends Component {
+
+  constructor() {
+    super();
+  }
+
+
+  render() {
+  }
+
+}
+
+export default BTTest;
